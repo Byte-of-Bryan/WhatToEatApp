affb252bfc9f8ba6a1fe368dd7a790a3ba1ff3a0
Bryan Morandi <bmorandi@depaul.edu>
2021-12-17 14:42:14 -0600

    Add project files.

---
+++ WhatToEat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WhatToEat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WhatToEat/Models/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace WhatToEat.Models
+{
+    public class Recipe
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Url { get; set; }
+        [AllowHtml]
+        public string contents { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
